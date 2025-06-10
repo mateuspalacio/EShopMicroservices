d129f905b2b0d3b935ad54b7033a8449150ac1e4
Mateus <mateuspalacio@gmail.com>
2025-06-10 15:55:45 -0300

    lesson 46

---
+++ source/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Here you would typically interact with a database or another service to create the product.
+        throw new NotImplementedException();
+    }
+}
