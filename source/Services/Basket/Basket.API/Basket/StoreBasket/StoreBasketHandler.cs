c879079926cb921ff33817ccb5a21e911e76e91f
Mateus <mateuspalacio@gmail.com>
2025-06-18 13:46:32 -0300

    174

--- source/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ source/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 
@@ -10,17 +12,29 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
         RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null.");
         RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName cannot be empty.");
-        RuleFor(x => x.Cart.Items).NotEmpty().WithMessage("Cart must contain at least one item.");
     }
 }
 
-public class StoreBasketCommandHandler(IBasketRepository repository)
+public class StoreBasketCommandHandler(
+    IBasketRepository repository,
+    DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
         var cart = command.Cart;
+        await DeductDiscount(cart, cancellationToken);
         await repository.StoreBasket(cart, cancellationToken);
         return new StoreBasketResult(cart.UserName);
+    }
+
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName },
+                cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
     }
 }
