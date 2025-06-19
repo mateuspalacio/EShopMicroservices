d76342a2ef14e475773653d6d2a77011cd8ea224
Mateus <mateuspalacio@gmail.com>
2025-06-19 14:21:53 -0300

    199

---
+++ source/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // Register API services here
+        // Example: services.AddControllers();
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // Configure API middleware here
+        // Example: app.UseRouting();
+        app.MapControllers();
+        return app;
+    }
+}
---
+++ source/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Domain.Exceptions;
+
+public class DomainException(string message)
+    : Exception($"Domain Exception: \"{message}\" throws from Domain Layer");
